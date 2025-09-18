47aab864f7331ca986941366f471e30dadf32e92
Luigi <luigi@ernesta.com>
2025-09-18 15:37:19 -0300

    Add booking data fixture for testing

---
+++ fixtures/bookingData.ts
@@ -0,0 +1,5 @@
+export const bookings = [
+  { first: 'John', last: 'Doe', email: 'john.doe@example.com', phone: '123456789012' },
+  { first: 'Jane', last: 'Smith', email: 'jane.smith@example.com', phone: '098765432123' },
+  { first: 'Mario', last: 'Rossi', email: 'mario.rossi@example.com', phone: '555555555555' },
+];
